b447cbd3397c8619c32875231642ee79b6260830
Ryosei Goto <swear1208a13@gmail.com>
2022-10-02 13:24:50 +0900

    ページ素材用のinterfaceの追加

---
+++ KrkrNovelist/ViewModels/PageMaterial/IMaterialViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reactive.Bindings;
+
+namespace KrkrNovelist.ViewModels.PageMaterial
+{
+    public interface IMaterialViewModel<T>
+    {
+        /// <summary>
+        /// 素材のデータ
+        /// </summary>
+        ReactiveProperty<T> Data { get; set; }
+    }
+}
