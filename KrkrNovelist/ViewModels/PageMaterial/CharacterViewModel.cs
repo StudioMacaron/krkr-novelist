2a620de0a61db6fe1733b15c11b92478f4a27d71
Ryosei Goto <swear1208a13@gmail.com>
2022-10-02 13:25:02 +0900

    CharacterViewModelを作成

---
+++ KrkrNovelist/ViewModels/PageMaterial/CharacterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reactive.Bindings;
+using System.ComponentModel;
+using KrkrNovelist.Models;
+using KrkrNovelist.ViewModels.PageMaterial;
+
+namespace KrkrNovelist.ViewModels.PageMaterial
+{
+    public class CharacterViewModel : IMaterialViewModel<Character>, INotifyPropertyChanged
+    {
+        public ReactiveProperty<Character> Data { get; set; } = new();
+
+        public CharacterViewModel(Character character)
+        {
+            this.Data.Value = character;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+}
